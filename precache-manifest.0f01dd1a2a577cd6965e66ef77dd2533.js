491292b2749bd68a8ac800ed43cbc6608c6e1a86
prescottsun <sunprescott@gmail.com>
2019-12-30 23:21:57 -0500

    Updates

---
+++ precache-manifest.0f01dd1a2a577cd6965e66ef77dd2533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dec7f1a0cd631a22785dc948a983606",
+    "url": "/city-stats/index.html"
+  },
+  {
+    "revision": "07e133386426c77c3240",
+    "url": "/city-stats/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "e81d7e3571c58c476a1f",
+    "url": "/city-stats/static/css/main.ca3d197b.chunk.css"
+  },
+  {
+    "revision": "07e133386426c77c3240",
+    "url": "/city-stats/static/js/2.e5bb05d1.chunk.js"
+  },
+  {
+    "revision": "a6fc7eeb02d4070a309a623123b2fc97",
+    "url": "/city-stats/static/js/2.e5bb05d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e81d7e3571c58c476a1f",
+    "url": "/city-stats/static/js/main.acedf494.chunk.js"
+  },
+  {
+    "revision": "7910bf117f5bf38ace44",
+    "url": "/city-stats/static/js/runtime-main.3665e45e.js"
+  }
+]);
